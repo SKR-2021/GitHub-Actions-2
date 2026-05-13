98e0b491defbadf0ca622e50afac88d42394671e
SKR-2021 <karunakar.dev2000@gmail.com>
2026-05-13 18:47:57 +0200

    GH_Runner

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  default = "dotmart"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "dso86s.xyz"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z09150472BEG29NERJFM7"
+  description = "description"
+}
